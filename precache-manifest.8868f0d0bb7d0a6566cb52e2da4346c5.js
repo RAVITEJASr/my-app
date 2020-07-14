92f53f68e45803e3bb9e7bce3304d0cc6303c53b
RAVITEJASr <ravitejas010408@gmail.com>
2020-07-14 19:44:41 +0530

    Updates

---
+++ precache-manifest.8868f0d0bb7d0a6566cb52e2da4346c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b016b8c81281d4bbf7cc2b8b9c1100",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "05e208eb3c146e53a395",
+    "url": "/my-app/static/css/main.e1f1a4f2.chunk.css"
+  },
+  {
+    "revision": "0a6652087263858040a6",
+    "url": "/my-app/static/js/2.7d89ecae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.7d89ecae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05e208eb3c146e53a395",
+    "url": "/my-app/static/js/main.b57bcaac.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
